1bc32fbf6fb2b815ada9ac1b2dce5ab87bb2703c
Eric Lee <ranye0719@gmail.com>
2023-07-08 20:05:21 +0800

    feat: able to update and delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ActionController::Base
+class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
@@ -12,6 +12,10 @@ class ArticlesController < ActionController::Base
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
@@ -22,6 +26,22 @@ class ArticlesController < ActionController::Base
             render 'new'
         end
 
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 
 end
